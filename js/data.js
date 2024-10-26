49f410312d3b2d7c55ec96b532050f1ba56d1558
Alvaro Martinez <almarseda@gmail.com>
2024-10-26 18:56:56 +0200

    Creado el script que pinta las plantas, su estilo y modificacion de data.js para actualizar imagenes

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ const plantas = [
   {
     id: 1,
     nombre: "Aloe Vera",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/7c/17/1a/7c171acc162c33d9cdb84edad06f52c8.jpg",
     descripcion: "Planta suculenta conocida por sus propiedades medicinales.",
     precio: 10.99,
     stock: 10
@@ -10,7 +10,7 @@ const plantas = [
   {
     id: 2,
     nombre: "Monstera Deliciosa",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/2b/a5/44/2ba5448091ae3d061a541d025ac6e2bb.jpg",
     descripcion: "Planta tropical con hojas grandes y divididas, ideal para interiores.",
     precio: 25.99,
     stock: 8
@@ -18,7 +18,7 @@ const plantas = [
   {
     id: 3,
     nombre: "Cactus",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/e0/d4/b4/e0d4b4ba91f211fad183d34733d7ae7c.jpg",
     descripcion: "Planta suculenta resistente, perfecta para principiantes.",
     precio: 5.99,
     stock: 15
@@ -26,7 +26,7 @@ const plantas = [
   {
     id: 4,
     nombre: "Lavanda",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/736x/f2/78/0b/f2780bad6101b8233dc024491eaacf97.jpg",
     descripcion: "Planta aromática que produce flores lilas y huele maravilloso.",
     precio: 12.99,
     stock: 7
@@ -34,7 +34,7 @@ const plantas = [
   {
     id: 5,
     nombre: "Palmera Kentia",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/1200x/22/19/52/221952846e32fcea1676f335c081683d.jpg",
     descripcion: "Palmera elegante que da un toque tropical a cualquier espacio.",
     precio: 30.99,
     stock: 5
@@ -42,7 +42,7 @@ const plantas = [
   {
     id: 6,
     nombre: "Ficus Lyrata",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/736x/5f/34/15/5f34153409a9ca474b3717bbc5f7b02c.jpg",
     descripcion: "Conocido como 'Fiddle Leaf Fig', es una planta de interior popular.",
     precio: 22.99,
     stock: 12
@@ -50,7 +50,7 @@ const plantas = [
   {
     id: 7,
     nombre: "Potos",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/736x/6e/30/27/6e302720a3cf63ba7302acee730ed039.jpg",
     descripcion: "Planta de interior fácil de cuidar y excelente para purificar el aire.",
     precio: 9.99,
     stock: 11
@@ -58,7 +58,7 @@ const plantas = [
   {
     id: 8,
     nombre: "Sansevieria",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/736x/57/aa/cb/57aacb9af8bc0cdf7822ef5c44287efd.jpg",
     descripcion: "Conocida como 'Lengua de Suegra', es ideal para principiantes.",
     precio: 15.99,
     stock: 9
@@ -66,7 +66,7 @@ const plantas = [
   {
     id: 9,
     nombre: "Helecho de Boston",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/51/ca/29/51ca29b06ed07e934e8b47910cd2340f.jpg",
     descripcion: "Helecho frondoso, excelente para purificar el aire en interiores.",
     precio: 13.99,
     stock: 14
@@ -74,7 +74,7 @@ const plantas = [
   {
     id: 10,
     nombre: "Bonsai Ficus",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/bb/42/37/bb4237d62e083633e9cc6ac60bb7ab67.jpg",
     descripcion: "Un bonsái clásico, símbolo de paz y calma.",
     precio: 45.99,
     stock: 3
@@ -82,7 +82,7 @@ const plantas = [
   {
     id: 11,
     nombre: "Suculenta Echeveria",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/736x/d0/76/a0/d076a00b7823ee1dd6985730c6f4868c.jpg",
     descripcion: "Planta pequeña y decorativa, ideal para interiores.",
     precio: 4.99,
     stock: 15
@@ -90,7 +90,7 @@ const plantas = [
   {
     id: 12,
     nombre: "Orquídea Phalaenopsis",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/736x/c8/e1/7c/c8e17c7a41ea3944e1c77aeb0eda808d.jpg",
     descripcion: "Orquídea elegante con flores duraderas.",
     precio: 19.99,
     stock: 6
@@ -98,7 +98,7 @@ const plantas = [
   {
     id: 13,
     nombre: "Hiedra Inglesa",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/736x/08/8f/6b/088f6b9dc633604c75cc9a94496d8b0a.jpg",
     descripcion: "Planta trepadora, perfecta para decorar interiores y exteriores.",
     precio: 7.99,
     stock: 10
@@ -106,7 +106,7 @@ const plantas = [
   {
     id: 14,
     nombre: "Hierbabuena",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/c4/46/17/c44617623b4264f4faad1c826b1ee2cf.jpg",
     descripcion: "Hierba aromática ideal para cocinar y bebidas.",
     precio: 3.99,
     stock: 14
@@ -114,7 +114,7 @@ const plantas = [
   {
     id: 15,
     nombre: "Calathea Orbifolia",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/2e/9d/cb/2e9dcbde99375f96e026cdb87f804fb3.jpg",
     descripcion: "Planta de hojas grandes y decorativas, ideal para interiores.",
     precio: 18.99,
     stock: 8
@@ -122,7 +122,7 @@ const plantas = [
   {
     id: 16,
     nombre: "Jade",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/26/b8/4f/26b84fd5ae08229e806dd5d6e9a3c6b9.jpg",
     descripcion: "Planta de buena suerte, fácil de cuidar.",
     precio: 9.99,
     stock: 12
@@ -130,7 +130,7 @@ const plantas = [
   {
     id: 17,
     nombre: "Dracaena Marginata",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/736x/35/62/ef/3562ef0d1323fced372652dd1fbef0b3.jpg",
     descripcion: "Planta de interior con hojas largas y delgadas.",
     precio: 16.99,
     stock: 5
@@ -138,7 +138,7 @@ const plantas = [
   {
     id: 18,
     nombre: "Palmera Areca",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/fa/2b/1f/fa2b1f71b8b97b760d892f80aabb3555.jpg",
     descripcion: "Palmera de interior que da un toque tropical.",
     precio: 21.99,
     stock: 6
@@ -146,7 +146,7 @@ const plantas = [
   {
     id: 19,
     nombre: "Crotón",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/736x/29/1b/6c/291b6caaeb72693b1c67770efdaee3fd.jpg",
     descripcion: "Planta colorida con hojas brillantes.",
     precio: 11.99,
     stock: 9
@@ -154,7 +154,7 @@ const plantas = [
   {
     id: 20,
     nombre: "Espatifilo",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/92/57/0a/92570ac583f408c973e429799b490fb2.jpg",
     descripcion: "Planta de interior conocida como Lirio de la Paz.",
     precio: 14.99,
     stock: 7
@@ -162,7 +162,7 @@ const plantas = [
   {
     id: 21,
     nombre: "Palma Bambú",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/3c/67/9e/3c679e0a38581c7ed30ef1fa092aeb1c.jpg",
     descripcion: "Palma pequeña ideal para interiores, con hojas delgadas y elegantes.",
     precio: 17.99,
     stock: 9
@@ -170,7 +170,7 @@ const plantas = [
   {
     id: 22,
     nombre: "Aglaonema",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/09/1f/d5/091fd5eaf9693d35da0eff4c07cfb6a7.jpg",
     descripcion: "Planta con hojas coloridas y fáciles de cuidar, ideal para interiores.",
     precio: 15.99,
     stock: 10
@@ -178,7 +178,7 @@ const plantas = [
   {
     id: 23,
     nombre: "Begonia Rex",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/2e/d5/68/2ed56893fd878c55eb34029ae71f9545.jpg",
     descripcion: "Planta con hojas coloridas y textura única.",
     precio: 12.99,
     stock: 8
@@ -186,7 +186,7 @@ const plantas = [
   {
     id: 24,
     nombre: "Haworthia",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/736x/9a/95/2b/9a952bf223af7a7e820ceaf1fc169c18.jpg",
     descripcion: "Pequeña suculenta con un patrón único en sus hojas.",
     precio: 6.99,
     stock: 15
@@ -194,7 +194,7 @@ const plantas = [
   {
     id: 25,
     nombre: "Fresia",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/736x/f8/7b/d4/f87bd4d5e77c0d196a37c2891ff700ba.jpg",
     descripcion: "Flor colorida y fragante, ideal para decoración.",
     precio: 13.99,
     stock: 5
@@ -202,7 +202,7 @@ const plantas = [
   {
     id: 26,
     nombre: "Planta Serpiente",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/736x/c1/86/6c/c1866cc349aba11450e5f37037b0d0ca.jpg",
     descripcion: "Planta fácil de cuidar, ideal para principiantes.",
     precio: 11.99,
     stock: 10
@@ -210,7 +210,7 @@ const plantas = [
   {
     id: 27,
     nombre: "Caléndula",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/39/30/c5/3930c5d36983cdf249a725997c482f69.jpg",
     descripcion: "Flor de color naranja vibrante, ideal para jardines.",
     precio: 7.99,
     stock: 12
@@ -218,7 +218,7 @@ const plantas = [
   {
     id: 28,
     nombre: "Philodendron Birkin",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/c6/ce/90/c6ce90c6a3d03476b2a25d385ff503a8.jpg",
     descripcion: "Planta de interior con hojas rayadas en blanco y verde.",
     precio: 19.99,
     stock: 6
@@ -226,7 +226,7 @@ const plantas = [
   {
     id: 29,
     nombre: "Zamioculca",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/66/ac/d1/66acd1b22a1d0ceb15b00b7d5c1040b5.jpg",
     descripcion: "Planta resistente y fácil de cuidar, ideal para interiores.",
     precio: 14.99,
     stock: 7
@@ -234,7 +234,7 @@ const plantas = [
   {
     id: 30,
     nombre: "Alocasia Polly",
-    imagen: "https://picsum.photos/150/200",
+    imagen: "https://i.pinimg.com/564x/46/ae/ef/46aeef371debdca67628f6b5cdf016d0.jpg",
     descripcion: "Planta de hojas grandes y decorativas, excelente para interiores.",
     precio: 18.99,
     stock: 9
--- js/main.js
+++ js/main.js
@@ -1,7 +1,47 @@
-const printOnePlant = function (plant, domubi) {
+/* <div class="planta_container">
+    <figure>
+        <img src="" alt="" >
+    </figure>
+        <div class="plant_desc">
+            <h3>Nombre Planta</h3>
+            <p>Descripcion Planta</p>
+        </div>
+        <div class="precio_cart">
+            <p> Precio </p>   <a class="btn"><i></a>
+        </div>
+</div> */
+const section = document.querySelector('#plants_expo')
+const printOnePlant = function (plant, domUbi) {
     const div = document.createElement('div')
     const figure = document.createElement('figure')
+    const img = document.createElement('img')
+    const h3 = document.createElement('h3')
+    const divDesc = document.createElement('div')
+    const divPrecio = document.createElement('div')
+    const desc = document.createElement('p')
+    const precio = document.createElement('p')
+    const a = document.createElement('a')
 
+    //Creacion de clases
+    div.classList.add('planta_container')
+    divDesc.classList.add('plant_desc')
+    divPrecio.classList.add('precio_cart')
+    a.classList.add('btn')
+
+    //Pintado de contenido
+    img.src = `${plant.imagen}`
+    h3.textContent = `${plant.nombre}`
+    desc.textContent = `${plant.descripcion}`
+    precio.textContent = `${plant.precio} €`
+    a.href = '#'
+    a.innerHTML = '<i class="fa-solid fa-cart-plus"></i>'
+
+    //Anexo de las partes
+    figure.appendChild(img)
+    divDesc.append(h3, desc)
+    divPrecio.append(precio, a)
+    div.append(figure, divDesc, divPrecio)
+    domUbi.append(div)
 
 }
 
@@ -9,3 +49,4 @@ const printAllPlants = function (plantList, domUbi) {
     plantList.forEach(planta => printOnePlant(planta, domUbi))
 }
 
+printAllPlants(plantas, section)
